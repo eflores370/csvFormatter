d990d48115e32dde709f8526d42bbe844d8b9d92
eflores370 <emanuelcflores@gmail.com>
2017-12-07 22:38:24 -0800

    pre

--- dataExtracter.py
+++ dataExtracter.py
@@ -1,13 +1,31 @@
 import re
 import sys
 
+tempClass = "CNIT"
+
 print("======================")
 print("1. CNITMajor")
 fileName = input("File Name: ")
 if fileName == "1":	
 	fileName = "CNITMajor.txt"
+
 file = open(fileName,"r")
 
+writeFile = open("CNITMajor.csv", "w+")
+
 for line in file:
-	if(re.search('CNIT', line)):
+	if(re.search('^' + re.escape(tempClass), line)):
 		print(line)
+		values = line.split("\xad")
+		left = values[0].strip().split("\xa0")
+		print("++++++++++++++")
+		print(left[0])
+		print(left[1])
+		print(values[1].strip())
+
+		writeFile.write(left[0] + ";")
+		writeFile.write(left[1] + ";")
+		writeFile.write(values[1].strip() + "\n")
+		
+
+
