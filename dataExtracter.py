6c278dddcb4e7b77cbaaa10ed145789cbdee88dd
eflores370 <emanuelcflores@gmail.com>
2017-12-07 23:07:54 -0800

    2

--- dataExtracter.py
+++ dataExtracter.py
@@ -1,27 +1,32 @@
 import re
 import sys
 
-tempClass = "CNIT"
+classArray = {"MATH", "CNIT", "CS"}
 
 print("======================")
-print("1. CNITMajor")
+print("1. CNIT Major")
+print("2. CS Major")
 fileName = input("File Name: ")
 if fileName == "1":	
 	fileName = "CNITMajor.txt"
+elif fileName == "2":
+	fileName = "ComputerScienceMajor.txt"
 
+# Open Text file
 file = open(fileName,"r")
 
-writeFile = open("CNITMajor.csv", "w+")
+# Open Write to file
+writeFile = open("output.csv", "w+")
 
 for line in file:
 	if(re.search('^' + re.escape(tempClass), line)):
-		print(line)
 		values = line.split("\xad")
 		left = values[0].strip().split("\xa0")
-		
-		writeFile.write(left[0] + ";")
-		writeFile.write(left[1] + ";")
+
+		writeFile.write(left[0] + "\t")
+		writeFile.write(left[1] + "\t")
 		writeFile.write(values[1].strip() + "\n")
-		
+
+print("Finished!")		
 
 
